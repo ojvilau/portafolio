50e9e0b7ab5db7860ecb8f7b30e4c4751fda844c
Orlando J. Vilau <ojvilau@gmail.com>
2021-11-25 03:29:03 -0500

    Override 'gray' color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          100: "#f7f7f7",
+          200: "#dddddd",
+          300: "#aaaaaa",
+          400: "#2b2b2b",
+          500: "#161616",
+        },
+      },
+    },
   },
   variants: {
     extend: {},
