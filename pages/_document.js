35cf2b1e02d7b25295f5326c008b5b75928c47fd
Orlando J. Vilau <ojvilau@gmail.com>
2021-11-26 13:46:55 -0500

    Add google font preload scripts and move favicon to main document

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,9 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;600;700&display=swap" rel="stylesheet" />
         </Head>
         <body className="font-outfit text-lg font-normal dark:bg-gray-500 bg-white transition duration-500 sm:text-xl">
